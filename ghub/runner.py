56dfe550c287107d448afcb0ea0104de782b571b
Vishal R <vishal27.rs@outlook.com>
2019-11-10 11:27:19 +0530

    Add ANSI color support in windows

--- ghub/runner.py
+++ ghub/runner.py
@@ -2,12 +2,14 @@
 from .ghubutils import Context, GHub
 from .cliutils import Interpreter
 from termcolor import colored
+import colorama
 
 import os
 
 
 def run_ghub():
     """Run GHub"""
+    colorama.init()
     print("Welcome to GHub - Browse GitHub like it is UNIX")
     print("Starting initial setup...")
     ghub = GHub()
