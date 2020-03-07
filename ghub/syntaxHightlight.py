8f929330d5cf22adb57daae6002d5154d73081e3
Maganth Seetharaman <mahanthathreyee@gmail.com>
2020-03-07 14:15:35 +0530

    Added check for terminal color support

--- ghub/syntaxHightlight.py
+++ ghub/syntaxHightlight.py
@@ -1,8 +1,17 @@
 from pygments import highlight
 from pygments.lexers import get_lexer_for_filename
-from pygments.formatters import Terminal256Formatter
+from pygments.formatters import TerminalFormatter, Terminal256Formatter
+import curses
+
 
 def printHighlightContent(fileName, content):
     lexer = get_lexer_for_filename(fileName)
-    
-    print(highlight(content, lexer, Terminal256Formatter()))
+    if getTerminalColorSupport() == 256:
+        print(highlight(content, lexer, Terminal256Formatter()))
+    else:
+        print(highlight(content, lexer, TerminalFormatter()))
+
+
+def getTerminalColorSupport():
+    curses.setupterm()
+    return curses.tigetnum("colors")
