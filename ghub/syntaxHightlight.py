b28e0b4434527ca0d6e9a8a35e6fc8adeaee4ea5
Maganth Seetharaman <mahanthathreyee@gmail.com>
2020-03-07 15:56:17 +0530

    added pager

--- ghub/syntaxHightlight.py
+++ ghub/syntaxHightlight.py
@@ -1,17 +1,34 @@
 from pygments import highlight
 from pygments.lexers import get_lexer_for_filename
 from pygments.formatters import TerminalFormatter, Terminal256Formatter
-import curses
+import tempfile
+import subprocess, sys, curses
 
 
 def printHighlightContent(fileName, content):
     lexer = get_lexer_for_filename(fileName)
     if getTerminalColorSupport() == 256:
-        print(highlight(content, lexer, Terminal256Formatter()))
+        paginate(highlight(content, lexer, Terminal256Formatter()))
     else:
-        print(highlight(content, lexer, TerminalFormatter()))
+        paginate(highlight(content, lexer, TerminalFormatter()))
 
 
 def getTerminalColorSupport():
     curses.setupterm()
     return curses.tigetnum("colors")
+
+
+def paginate(content):
+    path = tempfile.mkstemp()[1]
+
+    tmp_file = open(path, "a")
+    sys.stdout = tmp_file
+
+    print(content)
+
+    tmp_file.flush()
+    tmp_file.close()
+    p = subprocess.Popen(["less", "-R", path], stdin=subprocess.PIPE)
+    p.communicate()
+
+    sys.stdout = sys.__stdout__
